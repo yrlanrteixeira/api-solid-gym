e961850a9b71aa04cf083170ea570dac1286caee
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-07-25 10:56:48 -0300

    feat: Add pagination support to fetch user check-ins history

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,18 +6,14 @@ import dayjs from "dayjs";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
-  async findByUserIdOnDate(
-    userId: string,
-    date: Date
-  ): Promise<CheckIn[] | null> {
+  async findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn[] | null> {
     const startOfDay = dayjs(date).startOf("day");
     const endOfDay = dayjs(date).endOf("day");
 
     const checkOnSameDate = this.items.filter((checkIn) => {
       const checkInDate = dayjs(checkIn.created_at);
 
-      const isOnSameDay =
-        checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay);
+      const isOnSameDay = checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay);
 
       return checkIn.user_id === userId && isOnSameDay;
     });
@@ -27,6 +23,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
 
     return checkOnSameDate;
+  }
+
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    return Promise.resolve(this.items.filter((item) => item.user_id === userId).slice((page - 1) * 20, page * 20));
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
