646c9ace85b9037390e0ebdbc4c251731e551e35
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-07-25 11:04:45 -0300

    feat: Add countByUserId method to CheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -29,6 +29,9 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return Promise.resolve(this.items.filter((item) => item.user_id === userId).slice((page - 1) * 20, page * 20));
   }
 
+  countByUserId(userId: string): Promise<number> {
+    return Promise.resolve(this.items.filter((item) => item.user_id === userId).length);
+  }
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
