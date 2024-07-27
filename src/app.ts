9e383d60b8c67be485b42bdff1b06ce138e9dc5c
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-07-27 00:37:07 -0300

    feat: Add typings for fastify-jwt module and update dependencies and add route profile + authenticate token

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,19 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes);
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: "Validation error.", issues: error.format() });
+    return reply.status(400).send({ message: "Validation error.", issues: error.format() });
   }
 
   if (env.NODE_ENV !== "production") {
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -3,10 +3,7 @@ import { z } from "zod";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
 import { makeAuthenticateUseCase } from "@/factories/make-authenticate-use-case";
 
-export async function authenticate(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
   const autheticateBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
@@ -17,17 +14,25 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sub: user.id,
+      }
+    );
+
+    return reply.status(200).send({
+      token,
     });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
     }
-
     throw err;
   }
-
-  return reply.status(200).send();
 }
