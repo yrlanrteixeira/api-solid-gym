8f25d80db1c07ec78be94c8f807a3c8294af8886
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-08-02 13:20:38 -0300

    chore: Update npm dependencies and add supertest for e2e testing

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,33 @@
+import "dotenv/config";
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import { Environment } from "vitest";
+import { prisma } from "@/lib/prisma";
+
+function generateDataBaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "web",
   async setup() {
-    console.log("Setting up Prisma Test Environment...");
+    const schema = randomUUID();
+    const dataBaseURL = generateDataBaseURL(schema);
+
+    process.env.DATABASE_URL = dataBaseURL;
 
     return {
-      teardown() {
-        console.log("Tearing down Prisma Test Environment...");
+      async teardown() {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+        await prisma.$disconnect();
       },
     };
   },
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,25 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {});
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+    console.info("App is ready");
+  });
+
+  afterAll(async () => {
+    await app.close();
+    console.info("App is closed");
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Debug Teixeira",
+      email: "debugteixeira@example.com",
+      password: "123456",
+    });
+    console.info(response.body);
+    expect(response.status).toEqual(201);
+  });
+});
