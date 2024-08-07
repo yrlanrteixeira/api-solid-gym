706d21c0e6bc7e9ae46a15f13c4c8e2e22ae9759
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-08-07 10:45:48 -0300

    feat: Add routes for gym search and nearby functionality

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeNearbyGymsUseCase } from "@/factories/make-fetch-nearby-gyms-use-case";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine(
+      (value) => {
+        return Math.abs(value) <= 90;
+      },
+      { message: "Latitude must be between -90 and 90." }
+    ),
+
+    longitude: z.number().refine(
+      (value) => {
+        return Math.abs(value) <= 180;
+      },
+      { message: "Longitude must be between -180 and 180." }
+    ),
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body);
+
+  const searchGymsUseCase = makeNearbyGymsUseCase();
+
+  const { gyms } = await searchGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return reply.status(201).send({
+    gyms,
+  });
+}
