84d3072952576ecdd853d67e3c8faab861b6dfc2
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-08-08 12:04:54 -0300

    feat: Add e2e tests for check-in metrics

---
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,47 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+import { prisma } from "@/lib/prisma";
+
+describe("Check-in Metrics (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get the total count of check-in", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const user = await prisma.user.findFirstOrThrow();
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: "Academia 01",
+        latitude: -23.5664307,
+        longitude: -46.6026496,
+      },
+    });
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    });
+
+    const response = await request(app.server).get(`/check-ins/metrics`).set("Authorization", `Bearer ${token}`).send();
+
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.checkInsCount).toEqual(18);
+  });
+});
